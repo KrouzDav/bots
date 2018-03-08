439bfab9b39c9b6564b341660c7c2969e4f7bec5
PhoenixNewDragon <phoenixdragonvista@yandex.ru>
2018-03-08 12:41:43 +0400

    edit

--- bot_dnd/bot-dnd.js
+++ bot_dnd/bot-dnd.js
@@ -13,7 +13,7 @@ bot.onText(/start/, function (msg, match) {
     var text = '/roll<число>:  кидает кубик  с максимальным значением -  <число>\n' +
             '/roll20 /roll4 /roll6 /roll8 /roll10 \n' +
             '/stat: показывает накинутые статы для персонажа первого уровня (модификаторы)\n' +
-            '/gen <Имя>: генерирует персонажа Имя\n' +
+            '/genhero <Имя>: генерирует персонажа Имя\n' +
             '/attack <Имя> <оружие>: отображает вероятность попадания и урон\n';
 
     bot.sendMessage(msg.chat.id, text);
@@ -33,12 +33,8 @@ bot.onText(/countRoll(.+) (.+)/, function (msg, match) {
     bot.sendMessage(msg.chat.id, rollDiceCount(parseInt(match[1]), parseInt(match[2])));
 });
 
-bot.onText(/gen (.+)/, function (msg, match) {
+bot.onText(/genhero (.+)/, function (msg, match) {
     bot.sendMessage(msg.chat.id, generateHero(match[1]));
-});
-
-bot.onText(/gennpc (.+)/, function (msg, match) {
-    bot.sendMessage(msg.chat.id, generateNpc(match[1]));
 });
 
 bot.onText(/edithero (.+) (.+) (.+)/, function (msg, match) {
@@ -49,6 +45,10 @@ bot.onText(/hero (.+)/, function (msg, match) {
     bot.sendMessage(msg.chat.id, getHero(match[1]));
 });
 
+// bot.onText(/hero_(.+)/, function (msg, match) {
+//     bot.sendMessage(msg.chat.id, getHero(match[1]));
+// });
+
 bot.onText(/attack (.+) (.+)/, function (msg, match) {
     var res = attackHero(match[1], match[2]);
     bot.sendMessage(msg.chat.id, 'Атака (попадание): ' + res.attack + '\nУрон: ' + res.damage);
@@ -56,14 +56,39 @@ bot.onText(/attack (.+) (.+)/, function (msg, match) {
 
 bot.onText(/menu/, function (msg, match) {
     var text =
-        '/gen <Имя>: генерирует персонажа Имя\n' +
         '/edithero <Имя> <характеристика> <число>: увеличивает характеристику на указанное значение\n' +
-        '/hero <Имя>: показывает характеристики персонажа\n';
+        '/hero <Имя>: показывает характеристики персонажа\n' +
+        '/heroes Список героев \n' +
+        '/show_command_<hero>';
 
     bot.sendMessage(msg.chat.id, text);
 });
 
 bot.onText(/story (.+)/, function (msg, match) {
     bot.sendMessage(msg.chat.id, setStory(match[1]));
+});
+
+bot.onText(/heroes/, function (msg) {
+    var str = 'Убийцы королей: /Talos   /Mel   /Redstone' +
+        '';
+    bot.sendMessage(msg.chat.id, str);
+});
+
+bot.onText(/show_command_hero_(.+)/, function (msg, match) {
+    var str = show('command', 'hero', match[1]);
+    bot.sendMessage(msg.chat.id, str);
+});
+
+bot.onText(/show_hero_(.+)/, function (msg, match) {
+    var str = show('hero', match[1]);
+    bot.sendMessage(msg.chat.id, str);
+});
+
+bot.onText(/edit_hero_(.+)_(.+)_(.+)/, function (msg, match) {
+    bot.sendMessage(msg.chat.id, dataProcessing(match[1], match[2], match[3]));
+});
+
+bot.onText(/gen_hero_(.+)/, function (msg, match) {
+    bot.sendMessage(msg.chat.id, generateHero(match[1]));
 });
 
--- bot_dnd/functions/hero.js
+++ bot_dnd/functions/hero.js
@@ -21,18 +21,22 @@ generateHero = function (nameHero, classHero, levelHero) {
     json.level = levelHero;
 
     // Меняем данные на свои
-    json.characteristics.strenght = rollDice(20);
+    json.characteristics.strenght = rollDice(20)+1;
     json.characteristics.mod_strenght = ((json.characteristics.strenght-10.1)/2).toFixed();
-    json.characteristics.dexterity = rollDice(20);
+    json.characteristics.dexterity = rollDice(20)+1;
     json.characteristics.mod_dexterity = ((json.characteristics.dexterity-10.1)/2).toFixed();
-    json.characteristics.physique = rollDice(20);
+    json.characteristics.physique = rollDice(20)+1;
     json.characteristics.mod_physique = ((json.characteristics.physique-10.1)/2).toFixed();
-    json.characteristics.intelligence = rollDice(20);
+    json.characteristics.intelligence = rollDice(20)+1;
     json.characteristics.mod_intelligence = ((json.characteristics.intelligence-10.1)/2).toFixed();
-    json.characteristics.sapience = rollDice(20);
+    json.characteristics.sapience = rollDice(20)+1;
     json.characteristics.mod_sapience = ((json.characteristics.sapience-10.1)/2).toFixed();
-    json.characteristics.charisma = rollDice(20);
+    json.characteristics.charisma = rollDice(20)+1;
     json.characteristics.mod_charisma = ((json.characteristics.charisma-10.1)/2).toFixed();
+    // вычисляем другие данные
+    json.fight_characteristics.ca = 10 + parseInt(json.characteristics.mod_dexterity);
+    json.fight_characteristics.hp = 10 + parseInt(json.characteristics.mod_physique);
+    json.fight_characteristics.max_hp = 10 + parseInt(json.characteristics.mod_physique);
 
     setFileJson('../bot_dnd/heroes/' + json.name + '.json', json);
     // создаем nameHero.json
@@ -53,61 +57,75 @@ generateName = function () {
     return 'Классное имя';
 };
 
+getDict = function (name) {
+    var dict = require('../config/dict.json');
+    return dict[name] || name;
+};
+
 dataProcessing = function (name, essence, number) {
     var json = getFileJson('../bot_dnd/heroes/' + name + '.json');
-    switch (essence) {
-        case 'сила' : {
+    switch (getDict(essence)) {
+        case 'strenght' : {
+            json.characteristics.strenght = parseInt(json.characteristics.strenght) + parseInt(number*2);
             json.characteristics.mod_strenght = parseInt(json.characteristics.mod_strenght) + parseInt(number);
             break;
         }
-        case 'ловкость' : {
+        case 'dexterity' : {
+            json.characteristics.dexterity = parseInt(json.characteristics.dexterity) + parseInt(number*2);
             json.characteristics.mod_dexterity = parseInt(json.characteristics.mod_dexterity) + parseInt(number);
             break;
         }
-        case 'харизма' : {
+        case 'charisma' : {
+            json.characteristics.charisma = parseInt(json.characteristics.charisma) + parseInt(number*2);
             json.characteristics.mod_charisma = parseInt(json.characteristics.mod_charisma) + parseInt(number);
             break;
         }
-        case 'мудрость' : {
+        case 'sapience' : {
             json.characteristics.mod_sapience = parseInt(json.characteristics.mod_sapience) + parseInt(number);
             break;
         }
-        case 'интеллект' : {
+        case 'intelligence' : {
+            json.characteristics.intelligence = parseInt(json.characteristics.intelligence) + parseInt(number*2);
             json.characteristics.mod_intelligence = parseInt(json.characteristics.mod_intelligence) + parseInt(number);
             break;
         }
-        case 'телосложение' : {
+        case 'physique' : {
+            json.characteristics.physique = parseInt(json.characteristics.physique) + parseInt(number*2);
             json.characteristics.mod_physique = parseInt(json.characteristics.mod_physique) + parseInt(number);
             break;
         }
-        case 'опыт' : {
+        case 'exp' : {
             json.exp = parseInt(json.exp) + parseInt(number);
             break;
         }
-        case 'уровень' : {
+        case 'level' : {
             json.level = parseInt(json.level) + parseInt(number);
             break;
         }
-        case 'хп' : {
+        case 'hp' : {
             json.fight_characteristics.hp = parseInt(json.fight_characteristics.hp) + parseInt(number);
             break;
         }
-        case 'максхп' : {
+        case 'maxhp' : {
             json.fight_characteristics.max_hp = parseInt(json.fight_characteristics.max_hp) + parseInt(number);
             break;
         }
-        case 'кд' : {
+        case 'ca' : {
             json.fight_characteristics.ca = parseInt(json.fight_characteristics.ca) + parseInt(number);
             break;
         }
-        case 'золото' : {
+        case 'gold' : {
             json.gold = parseInt(json.gold) + parseInt(number);
+            break;
+        }
+        default : {
+            json[getDict(essence)] = parseInt(json[getDict(essence)]) + parseInt(number);
             break;
         }
     }
     setFileJson('../bot_dnd/heroes/' + name + '.json', json);
 
-    return name + '\n' + essence + ': ' + number;
+    return name + '\n' + getDict(essence) + ': ' + number;
 };
 
 getHero = function (name) {
@@ -129,42 +147,40 @@ getHero = function (name) {
         'Золото: ' + json.gold + '\n\n';
 };
 
-generateNpc = function (nameHero, classHero, levelHero) {
-    var json = {};
-    nameHero = nameHero || generateName();
-    classHero = classHero || 'warrior';
-    levelHero = levelHero || 1;
-    // Устаналиваем данные на случай, если они не указаны в предусловии
-    // Читаем default_hero
-    json = getFileJson('../bot_dnd/npc/default_npc.json');
-    json.name = nameHero;
-    json.class = classHero;
-    json.level = levelHero;
+show = function (type1, type2, type3) {
+    var str = '';
+    if (type1 === 'command') {
+        // Показывает команды
+        if (typeof type2 !== 'undefined') {
+            switch (type2) {
+                case 'hero' : {
+                    // Если задан герой, то Показывает команды для героя type3
+                    var hero = type3;
+                    str = '/show_hero_' + hero + ' \n\n' +
+                        'Сила +1: /edit_hero_' + hero + '_strenght_1\n\n' +
+                        'Ловкость +1: /edit_hero_' + hero + '_dexterity_1\n\n' +
+                        'Телосложение +1: /edit_hero_' + hero + '_physique_1\n\n' +
+                        'Интеллект +1: /edit_hero_' + hero + '_intelligence_1\n\n' +
+                        'Мудрость +1: /edit_hero_' + hero + '_sapience_1\n\n' +
+                        'Харизма +1: /edit_hero_' + hero + '_charisma_1\n\n' +
+                        'хп +1: /edit_hero_' + hero + '_hp_1\n\n' +
+                        'максимальное хп +1: /edit_hero_' + hero + '_maxhp_1\n\n' +
+                        'кд +1: /edit_hero_' + hero + '_ca_1\n\n' +
+                        'опыт +50: /edit_hero_' + hero + '_exp_50\n\n' +
+                        'золото +10: /edit_hero_' + hero + '_gold_10\n\n';
+                    break;
+                }
+                default : {
 
-    // Меняем данные на свои
-    json.characteristics.strenght = rollDice(20);
-    json.characteristics.mod_strenght = ((json.characteristics.strenght-10.1)/2).toFixed();
-    json.characteristics.dexterity = rollDice(20);
-    json.characteristics.mod_dexterity = ((json.characteristics.dexterity-10.1)/2).toFixed();
-    json.characteristics.physique = rollDice(20);
-    json.characteristics.mod_physique = ((json.characteristics.physique-10.1)/2).toFixed();
-    json.characteristics.intelligence = rollDice(20);
-    json.characteristics.mod_intelligence = ((json.characteristics.intelligence-10.1)/2).toFixed();
-    json.characteristics.sapience = rollDice(20);
-    json.characteristics.mod_sapience = ((json.characteristics.sapience-10.1)/2).toFixed();
-    json.characteristics.charisma = rollDice(20);
-    json.characteristics.mod_charisma = ((json.characteristics.charisma-10.1)/2).toFixed();
+                }
+            }
 
-    setFileJson('../bot_dnd/npc/' + json.name + '.json', json);
-    // создаем nameNpc.json
-
-    // Выводим информацию о созданном персонаже
-    return 'Создан персонаж ' + json.name + '\n' +
-        'Сила: ' + json.characteristics.mod_strenght + '\n' +
-        'Ловкость: ' + json.characteristics.mod_dexterity + '\n' +
-        'Телосложение: ' + json.characteristics.mod_physique + '\n' +
-        'Интеллект: ' + json.characteristics.mod_intelligence + '\n' +
-        'Мудрость: ' + json.characteristics.mod_sapience + '\n' +
-        'Харизма: ' + json.characteristics.mod_charisma;
-
+        } else {
+            // Показывает общие команды
+        }
+    } else {
+        // Показывает информацию о герое type1
+        str = getHero(type2);
+    }
+    return str;
 };
